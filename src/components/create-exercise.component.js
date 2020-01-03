1c203ada295c208e8dcca1f3cc7a28c40079eb2a
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-01-03 13:58:44 -0600

    added axios connection to tie front and back ends

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -22,10 +23,15 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername(e) {
@@ -64,6 +70,9 @@ export default class CreateExercise extends Component {
 
         console.log(exercise)
 
+        axios.post('http://localhost:5000/exercises/add', exercise)
+        .then(res => console.log(res.data));
+
         window.location = "/";
     }
 
@@ -93,7 +102,6 @@ export default class CreateExercise extends Component {
                 <div className="form-group">
                     <label>Description: </label>
                     <input type="text"
-                    required
                     className="form-control"
                     value={this.state.description}
                     onChange={this.onChangeDescription}
